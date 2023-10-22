08817a19d59e7d9fe8481d0ab2e6ad6759417969
harutas <96802323+harutas@users.noreply.github.com>
2023-10-22 23:55:12 +0900

    feat: pagenation

--- Views/problems.php
+++ Views/problems.php
@@ -4,6 +4,21 @@ require_once("../Models/Problem.php");
 
 $problemController = new Controllers\ProblemController();
 $problems = $problemController->getProblems();
+
+define("MAX_PER_PAGE", 5);
+
+$problems_num = count($problems);
+$max_page = ceil($problems_num / MAX_PER_PAGE);
+
+if (!isset($_GET["page_id"])) {
+  $now = 1;
+} else {
+  $now = $_GET["page_id"];
+}
+
+$start_page_num = ($now - 1) * MAX_PER_PAGE;
+$display_problems = array_slice($problems, $start_page_num, MAX_PER_PAGE, true);
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +50,7 @@ $problems = $problemController->getProblems();
         </thead>
         <tbody>
           <?php
-          foreach ($problems as $problem) : ?>
+          foreach ($display_problems as $problem) : ?>
             <?php $id = $problem->id ?>
             <tr style="cursor: pointer;" onclick="navigateToProblem(<?php echo $id ?>)">
               <th scope="row"><?php echo $problem->id ?></th>
@@ -51,19 +66,46 @@ $problems = $problemController->getProblems();
     <div class="d-flex justify-content-center">
       <nav aria-label="Page navigation example">
         <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
+          <?php
+          if ($now > 1) {
+            echo '
+              <li class="page-item">
+                <a class="page-link" href="problems.php?page_id=1" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>';
+          } else {
+            echo '
+              <li class="page-item disabled">
+                <a class="page-link" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>';
+          }
+          for ($i = 1; $i <= $max_page; $i++) {
+            if ($i == $now) {
+              echo '<li class="page-item disabled"><a class="page-link">' . $i . '</a></li>';
+            } else {
+              echo '<li class="page-item"><a class="page-link" href="problems.php?page_id=' . $i . '">' . $i . '</a></li>';
+            }
+          }
+
+          if ($now < $max_page) {
+            echo '
+                <li class="page-item">
+                  <a class="page-link" href="problems.php?page_id=' . $max_page . '" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>';
+          } else {
+            echo '
+                <li class="page-item disabled">
+                  <a class="page-link" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>';
+          }
+          ?>
         </ul>
       </nav>
     </div>
